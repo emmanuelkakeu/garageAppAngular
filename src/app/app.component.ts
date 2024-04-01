3bc8fc101f59114ff5860b7c06782f1ff72c4ee5
emmanuelkakeu <emmanuelkakeu2@gmail.com>
2024-04-01 18:11:55 +0200

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit , NgZone} from '@angular/core';
+//mport { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+import { GarageService } from './garage.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers:[GarageService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'garageAppAng';
+  cars! : any
+
+  result: string="";
+
+
+  constructor(private garageService : GarageService, private ngZone: NgZone){
+
+  }
+
+  startAsyncOperation() {
+
+    this.ngZone.run(() => {
+      // Votre opération asynchrone ici
+      this.result = 'En cours...';
+
+      setTimeout(() => {
+        this.result = 'Opération terminée';
+      }, 2000);
+    });
+  }
+
+  ngOnInit() {
+    console.log('on init....')
+    this.garageService.getCars().subscribe((datas) => {
+      this.cars = datas;})
+  }
+
 }
